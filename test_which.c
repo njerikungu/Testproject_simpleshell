bf4f9807e89c5b1d5a837653afeae6f6683f42d1
JeremyWarui <69347073+JeremyWarui@users.noreply.github.com>
2022-11-15 14:37:01 +0000

    updated test_which file

--- test_which.c
+++ test_which.c
@@ -1,40 +1,44 @@
 #include "test_shell.h"
 
 /**
- * _which - fleshes out command by appending it to a matching PATH directory
+ * _which - flushes out command by appending it to a matching PATH directory
  * @str: first command user typed into shell (e.g. "ls" if user typed "ls -l")
  * @env: environmental variable
  * Return: a copy of fleshed out command (e.g. "/bin/ls" if originally "ls")
  */
 char *_which(char *str, list_t *env)
 {
-	char *pth, *cat = NULL, **toks;
+
+	/*** NOTE: REPLACE "cAT" WITH "CAT_sTR"* ***/
+	/*** NOTE: REPLACE "TOKS" WITH "TOKENS"* ***/
+	
+	char *path, *cat_str = NULL, **tokens;
 	int i = 0;
 
-	/* get and tokenize PATH directories, then free original string */
-	pth = get_env("PATH", env);
-	toks = c_str_tok(pth, ":");
-	free(pth);
+	/* tokenize path dirs */
+	path = get_env("PATH", env);
+	tokens = c_str_tok(path, ":");
+	free(path);
 
 	/* append "/cmd" to each token to see it's legit */
 	i = 0;
-	while (toks[i] != NULL)
+	while (tokens[i] != NULL)
 	{
-		if (toks[i][0] == '\0')
-			cat = getcwd(cat, 0);
+		if (tokens[i][0] == '\0')
+			cat_str = getcwd(cat_str, 0);
 		else
-			cat = _strdup(toks[i]);
-		cat = _strcat(cat, "/");
-		cat = _strcat(cat, str);
-		if (access(cat, F_OK) == 0)
+			cat_str = _strdup(tokens[i]);
+		cat_str = _strcat(cat_str, "/");
+		cat_str = _strcat(cat_str, str);
+		if (access(cat_str, F_OK) == 0)
 		{
 			/* free tokens before returning legit fleshed path */
-			free_double_ptr(toks);
-			return (cat);
+			free_double_ptr(tokens);
+			return (cat_str);
 		}
-		free(cat); /* free concatenated string if cmd is never found */
+		free(cat_str); /* free concatenated string if cmd is never found */
 		i++;
 	}
-	free_double_ptr(toks);
+	free_double_ptr(tokens);
 	return (str); /* return string if not found; won't pass execve */
 }
